2c6ddf78c7d9e05a039ee7dce8587559a97efee7
Messias Cardoso <messiascardoso@gmail.com>
2023-03-28 23:49:54 -0300

    setup inicial e add use state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo  ${reverseClass}`} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Contador: {counter}</h1>
+
+        <button type="button" onClick={handleClick}>
+          Reverse {reverseClass}
+        </button>
+        <button type="button" onClick={increment}>
+          Increment
+        </button>
       </header>
     </div>
   );
