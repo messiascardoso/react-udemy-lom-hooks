800b89526e0ff2308db81e3a9479de6a8ac9d383
Messias Cardoso <messiascardoso@gmail.com>
2023-04-15 12:58:46 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -5,95 +5,56 @@ import React, {
   useRef,
   Children,
   useContext,
+  useReducer,
 } from 'react';
 import './App.css';
 //import P from 'prop-types';
 
 const globalState = {
-  title: 'Título do contexto',
-  body: 'Conteudo do body',
+  title: 'O titulo',
+  body: 'Conteudo body',
   counter: 0,
 };
 
-const GlobalContext = React.createContext();
-
-// eslint-disable-next-line
-const Div = ({ children }) => {
-  return (
-    <div className="App" style={{ backgroundColor: 'red' }}>
-      <H1 />
-      <P />
-    </div>
-  );
-};
-// eslint-disable-next-line
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title },
-  } = theContext;
-  return <h1>{title}</h1>;
-};
-// eslint-disable-next-line
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  const { contextState, setContextState } = theContext;
-  console.log(theContext);
-  return (
-    // Para atualizar um state de um objeto precisa fazer o spread e depois
-    // passar a chave a ser alterada como segundo parametro ou passar uma function conforme exemplo abaixo.
-    <>
-      {/* Update state with spread */}
-      <button
-        onClick={() =>
-          setContextState({
-            ...contextState,
-            counter: contextState.counter + 1,
-          })
-        }
-      >
-        {contextState.counter}
-      </button>
-      {/* Update state with function */}
-      <button
-        onClick={() =>
-          setContextState((prevState) => ({
-            ...prevState,
-            counter: prevState.counter + 1,
-          }))
-        }
-      >
-        {contextState.counter}
-      </button>
-    </>
-  );
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda');
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join() };
+    }
+  }
+  console.log('NENHUMA ACAO ENCONTRADA');
+  return { ...state };
 };
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
-
+  //dispatch => disparar acoes que chama a function reducer
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
-      <Div />
-    </GlobalContext.Provider>
+    <div>
+      <pre style={{ backgroundColor: '#dedede' }}>
+        {JSON.stringify(state, null, 4)}
+      </pre>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-Br'),
+          })
+        }
+      >
+        Muda text
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>invert</button>
+      <button onClick={() => dispatch({ type: '' })}>Sem Action</button>
+    </div>
   );
 }
 
 export default App;
-
-/**
- * State como funciona
- * ```jsx
-const context = {
-    contextState: {
-        title: 'Olá',
-        body: 'body conteudo'
-    },
-    setState: function(param) {
-        this.contextState = param
-    }
-}
-```
-context.setState({...context.contextState, title: 'Alterado'})
-
- */
