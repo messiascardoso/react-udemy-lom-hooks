5a5d2c0c2acfa0923c0eda08b0a6c8c21b3e47af
Messias Cardoso <messiascardoso@gmail.com>
2023-04-15 11:07:58 -0300

    Hook useContext parte1

--- src/App.js
+++ src/App.js
@@ -1,78 +1,81 @@
-import { useEffect, useState, useMemo, useRef } from 'react';
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useRef,
+  Children,
+  useContext,
+} from 'react';
 import './App.css';
-import P from 'prop-types';
+//import P from 'prop-types';
 
-const Post = ({ post, handleClick }) => {
-  console.log('filho renderizou');
+const globalState = {
+  title: 'Título do contexto',
+  body: 'Conteudo do body',
+  counter: 0,
+};
+
+const GlobalContext = React.createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
   return (
-    <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p>{post.body}</p>
+    <div className="App" style={{ backgroundColor: 'red' }}>
+      <H1 />
+      <P />
     </div>
   );
 };
-
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
+// eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title },
+  } = theContext;
+  return <h1>{title}</h1>;
+};
+// eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const { contextState, setContextState } = theContext;
+  console.log(theContext);
+  return (
+    // Para atualizar um state de um objeto precisa fazer o spread e depois
+    // passar a chave a ser alterada como segundo parametro ou passar uma function conforme exemplo abaixo.
+    <>
+      {/* Update state with spread */}
+      <button
+        onClick={() =>
+          setContextState({
+            ...contextState,
+            counter: contextState.counter + 1,
+          })
+        }
+      >
+        {contextState.counter}
+      </button>
+      {/* Update state with function */}
+      <button
+        onClick={() =>
+          setContextState((prevState) => ({
+            ...prevState,
+            counter: prevState.counter + 1,
+          }))
+        }
+      >
+        {contextState.counter}
+      </button>
+    </>
+  );
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const inputRef = useRef(null);
-  const contadorRef = useRef(null);
-  console.log('Pai renderizou');
-
-  //Component did mount
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((result) => result.json())
-      .then((result2) => setPosts(result2));
-  }, []);
-
-  useEffect(() => {
-    console.log(inputRef.current);
-    inputRef.current.focus();
-  }, [value]);
-
-  useEffect(() => {
-    // Quando for alterado o valor de useRef()
-    // não causa uma nova reinderizacao
-    contadorRef.current++;
-  });
-
-  const handleOnclick = (value) => {
-    setValue(value);
-  };
+  const [contextState, setContextState] = useState(globalState);
 
   return (
-    <div className="App">
-      <h6>Renderizou: {contadorRef.current}x</h6>
-      <p>
-        <input
-          ref={inputRef}
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-      {/* Pode memorizar um componente ou valores */}
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => (
-            <Post key={post.id} post={post} handleClick={handleOnclick} />
-          ))
-        );
-      }, [posts])}
-
-      {posts.length <= 0 && <p>Não há registros</p>}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
